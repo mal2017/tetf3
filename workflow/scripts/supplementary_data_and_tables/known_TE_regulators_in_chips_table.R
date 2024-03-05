3679f6aa05149aba5ec5ee32414ea943d90df2f2
mal2017 <>
2024-03-05 18:09:46 -0500

    a couple supp tables

---
+++ workflow/scripts/supplementary_data_and_tables/known_TE_regulators_in_chips_table.R
@@ -0,0 +1,44 @@
+library(tidyverse)
+library(GenomicRanges)
+library(gt)
+library(plyranges)
+
+te_regulators <- read_tsv("results/resources/pirna_pathway.tsv")
+lkup <-read_tsv("results/resources/gene_symbol_lookup.tsv.gz")
+
+d <- read_rds("results/pirna/encode_peaks_dist_to_pirna.gr.rds") |>
+  filter(ChIP !="gro")
+
+oi <- c("arx","piwi","Panx","nxf2","mael",
+        "fs(1)Yb",
+        "shu","SoYb","vret","armi","mino","zuc","Gasz","daed","papi",
+        "cuff","moon","del","Boot","rhi","Nxf3",
+        "vas","qin","squ","tapas","BoYb","krimp","spn-E","tej","AGO3","aub")
+
+oi <- filter(lkup, gene_symbol %in% oi) |> dplyr::select(gene_id=gene_ID,gene_symbol)
+
+dx <- d |>
+  as_tibble() |>
+  inner_join(oi) |>
+  dplyr::select(feature="gene_id",gene_symbol,ChIP,distance) |>
+  distinct() |>
+  pivot_wider(names_from = ChIP, values_from = distance) |>
+  dplyr::rename(`gene ID`=feature, `symbol`=gene_symbol)
+
+gx <- gt(dx)
+
+gx <- tab_style(gx,style = cell_text(style="italic"),
+          locations = cells_body(columns=c("symbol")))
+
+gx <- tab_style(gx,style = cell_text(style="italic"),
+                locations = cells_column_labels(columns=-c("gene ID","symbol")))
+
+gx <- gx |>
+  data_color(columns=-c("gene ID","symbol"),
+             direction="row",
+             method="bin", palette = "Greens",reverse=T,bins=c(0,100,500,1000,5000,1e7))
+
+gx
+
+dir.create("results/tables/")
+gtsave(gx,"results/tables/known_TE_regulators_in_chip.table.docx")
