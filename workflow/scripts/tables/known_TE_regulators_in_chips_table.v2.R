688fb48fef34efb44db4fafe91ed9481d44358ba
mal2017 <>
2024-04-07 13:42:09 -0400

    table with pirna gene/encode prox

--- workflow/scripts/tables/known_TE_regulators_in_chips_table.v2.R
+++ workflow/scripts/tables/known_TE_regulators_in_chips_table.v2.R
@@ -13,10 +13,8 @@ oi <- c("arx","piwi","Panx","nxf2","mael",
         "fs(1)Yb",
         "shu","SoYb","vret","armi","mino","zuc","Gasz","daed","papi",
         "cuff","moon","del","Boot","rhi","Nxf3",
-        "vas","qin","squ","tapas","BoYb","krimp","spn-E","tej","AGO3","aub")
-
-oi <- c("egg","mael","nxf2","Nxt1","arx","Su(var)205","piwi")
-
+        "vas","qin","squ","tapas","BoYb","krimp","spn-E","tej","AGO3","aub",
+        "egg","mael","nxf2","Nxt1","arx","Su(var)205","piwi") |> unique()
 
 oi <- filter(lkup, gene_symbol %in% oi) |> dplyr::select(gene_id=gene_ID,gene_symbol)
 
@@ -26,7 +24,7 @@ dx <- d |>
   inner_join(oi) |>
   dplyr::select(feature="gene_id",gene_symbol,ChIP,distance) |>
   distinct() |>
-  filter(distance < 5000) |>
+  #filter(distance < 5000) |>
   pivot_wider(names_from = ChIP, values_from = distance) |>
   dplyr::rename(`gene ID`=feature, `symbol`=gene_symbol) |>
   dplyr::select(-`gene ID`)
@@ -39,10 +37,11 @@ gx <- tab_style(gx,style = cell_text(style="italic"),
 gx <- tab_style(gx,style = cell_text(style="italic"),
                 locations = cells_column_labels(columns=-c("symbol")))
 
-gx <- gx |>
-  data_color(columns=-c("symbol"),
-             direction="row",
-             method="bin", palette = "Greens",reverse=T,bins=c(0,100,500,1000,5000,1e7))
+#gx <- gx |>
+#  data_color(columns=-c("symbol"),
+#             direction="row",
+#             method="bin", palette = "Greens",reverse=T,bins=c(0,100,500,1000,5000,1e7))
 
 dir.create("results/tables/")
-gtsave(gx,"results/tables/table_te_regulator_chip_prox.docx")
+gtsave(gx,snakemake@output$docx)
+gtsave(gx,"~/Downloads/test.docx")
