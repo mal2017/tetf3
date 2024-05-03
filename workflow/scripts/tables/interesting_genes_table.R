b96690c01db31293a9eaef58a1918c7a6987cba3
mal2017 <>
2024-05-03 08:22:14 -0400

    tweak interesting genes table

--- workflow/scripts/tables/interesting_genes_table.R
+++ workflow/scripts/tables/interesting_genes_table.R
@@ -7,7 +7,7 @@ library(writexl)
 mods <- read_tsv("upstream/final-models.collected-info.tsv.gz")
 
 
-# genes with more than 1 coex te
+# genes with at least 1 coex te
 n_coex <- mods |> 
   filter(significant_x) |>
   dplyr::select(gene_symbol,feature.y) |>
@@ -15,7 +15,7 @@ n_coex <- mods |>
   group_by(gene_symbol) |>
   summarise(n=n(),.groups = "drop")
 
-more_than_1_coex <- n_coex |> filter(n > 1)
+more_than_1_coex <- n_coex |> filter(n >= 1)
 
 # TEs with number of coexpressed TEs > pan
 more_coex_tes_than_pan <- n_coex |> filter(n>pull(filter(n_coex,gene_symbol == "pan"),n))
@@ -25,19 +25,19 @@ med_abs_scores <-mods |>
   filter(significant_x) |>
   group_by(model,feature.x,gene_symbol) |>
   summarise(across(c(estimate.qnorm),.fns=list(median_abs=~median(abs(.x))),.names = "{.fn}_{col}"),n=n()) |>
-  ungroup() |>
-  filter(n >= 3)
+  ungroup()
 
 pan_male_med_abs_coex <- pull(filter(med_abs_scores,gene_symbol == "pan" & model=="male"),median_abs_estimate.qnorm)
 pan_female_med_abs_coex <- pull(filter(med_abs_scores,gene_symbol == "pan" & model=="female"),median_abs_estimate.qnorm)
 
-male_med_abs_greater_than_pan <- med_abs_scores |> filter(model == "male" & median_abs_estimate.qnorm > pan_male_med_abs_coex)
-female_med_abs_greater_than_pan <- med_abs_scores |> filter(model == "female" & median_abs_estimate.qnorm > pan_male_med_abs_coex)
+male_med_abs_greater_than_pan <- med_abs_scores |> filter(model == "male" & median_abs_estimate.qnorm >= pan_male_med_abs_coex)
+female_med_abs_greater_than_pan <- med_abs_scores |> filter(model == "female" & median_abs_estimate.qnorm >= pan_male_med_abs_coex)
+uniq_greater_than_pan <- tibble(unique(c(male_med_abs_greater_than_pan$gene_symbol,female_med_abs_greater_than_pan$gene_symbol)))
 
-
-res <- list(`genes with more than 1 coexpressed TE`=more_than_1_coex,
-     `genes with more coexpressed TEs than pan`=more_coex_tes_than_pan,
-     `genes with higher male median abs. coex score than pan`=male_med_abs_greater_than_pan,
-     `genes with higher female median abs. coex score than pan`=female_med_abs_greater_than_pan)
+res <- list(`at least 1 coex TE`=more_than_1_coex,
+     `more coex TEs vs pan`=more_coex_tes_than_pan,
+     `higher M median abs score vs pan`=male_med_abs_greater_than_pan,
+     `higher F median abs score vs pan`=female_med_abs_greater_than_pan,
+     `higher median abs score vs pan either sex`=uniq_greater_than_pan)
 
 write_xlsx(res,snakemake@output$xlsx)
