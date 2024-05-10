c90f0e515e2b241c9267e15536a22fea9a978e7d
mal2017 <>
2024-05-10 11:20:05 -0400

    intersect te regs with phylosignal hits in table

--- workflow/scripts/tables/phylosignal_table.R
+++ workflow/scripts/tables/phylosignal_table.R
@@ -9,9 +9,15 @@ fully_filtered <- read_tsv("results/phylosignal/phylosignal_filtered_hits.tsv.gz
   dplyr::select(TF) |>
   distinct()
 
+te_regs <- read_tsv("results/resources/pirna_pathway.tsv")
+
+te_regs_intersection <- fully_filtered |>
+  filter(TF %in%te_regs$gene_symbol)
+
 x <- list(`coexpression score phyloSignal results`=read_rds("results/phylosignal/phylosignal_df.rds") |> filter(metric!="Lambda"),
      `Unr RIP-seq phyloSignal`=read_rds("results/ripseq/unr_ripseq_phylosignal.tbl.rds"),
-     `>=3 significant tests M or F`=fully_filtered)
+     `>=1 significant tests M or F`=fully_filtered,
+     `intersection with piRNA regulators`=te_regs_intersection)
 
 
 write_xlsx(x,snakemake@output$xlsx)
