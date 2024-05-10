d08ba5ba7bb9c89bb784d6e159ec036b365fc89f
mal2017 <>
2024-05-10 11:35:20 -0400

    add test of zad enrichment to phylosignal

--- workflow/scripts/tables/phylosignal_table.R
+++ workflow/scripts/tables/phylosignal_table.R
@@ -11,13 +11,34 @@ fully_filtered <- read_tsv("results/phylosignal/phylosignal_filtered_hits.tsv.gz
 
 te_regs <- read_tsv("results/resources/pirna_pathway.tsv")
 
+zads <- read_tsv("results/resources/zad_genes.tsv")
+
 te_regs_intersection <- fully_filtered |>
   filter(TF %in%te_regs$gene_symbol)
+
+zad_intersection <- fully_filtered |>
+  filter(TF %in%zads$gene_symbol)
+
+zad_cont <- read_rds("results/phylosignal/phylosignal_df.rds") |>
+  filter(metric!="Lambda" & score_type=="score") |>
+  group_by(sex,TF) |>
+  summarise(significance=if_else(any(padj < 0.1),"sig","n.s.")) |>
+  mutate(is_zad = if_else(TF %in% zads$gene_symbol,"ZAD","other")) |>
+  group_by(is_zad,significance) |>
+  summarise(n=n(),.groups = "drop") |>
+  arrange(desc(significance)) |>
+  pivot_wider(names_from = is_zad, values_from = n)
+
+zad_fish <- column_to_rownames(zad_cont,"significance") |>
+  fisher.test() |>
+  broom::tidy()
 
 x <- list(`coexpression score phyloSignal results`=read_rds("results/phylosignal/phylosignal_df.rds") |> filter(metric!="Lambda"),
      `Unr RIP-seq phyloSignal`=read_rds("results/ripseq/unr_ripseq_phylosignal.tbl.rds"),
      `>=1 significant tests M or F`=fully_filtered,
-     `intersection with piRNA regulators`=te_regs_intersection)
+     `intersection with piRNA regulators`=te_regs_intersection,
+     `intersection with ZADs`=zad_intersection,
+     `ZAD enrichment test`=zad_fish)
 
 
 write_xlsx(x,snakemake@output$xlsx)
