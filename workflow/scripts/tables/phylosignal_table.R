b2cbb15300038b9058e9b204fba9cdd69d8c3c13
mal2017 <>
2024-05-10 15:27:27 -0400

    export controls as their own pages in the phylosignal supp data

--- workflow/scripts/tables/phylosignal_table.R
+++ workflow/scripts/tables/phylosignal_table.R
@@ -4,6 +4,10 @@ source(Sys.getenv("R_PROFILE"))
 library(tidyverse)
 library(writexl)
 
+main_ps_ctrl <- read_rds("results/phylosignal/phylosignal_df.rds") |> filter(score_type == "control")
+
+unr_ps_ctrl <- read_rds("results/ripseq/unr_ripseq_phylosignal.tbl.rds") |> filter(coef %in% c("bm","random"))
+
 fully_filtered <- read_tsv("results/phylosignal/phylosignal_filtered_hits.tsv.gz") |>
   filter(score_type == "score" & n_tests_sig >=1) |>
   dplyr::select(TF) |>
@@ -36,6 +40,8 @@ zad_fish <- column_to_rownames(zad_cont,"significance") |>
 x <- list(`coexpression score phyloSignal results`=read_rds("results/phylosignal/phylosignal_df.rds") |> filter(metric!="Lambda"),
      `Unr RIP-seq phyloSignal`=read_rds("results/ripseq/unr_ripseq_phylosignal.tbl.rds"),
      `>=1 significant tests M or F`=fully_filtered,
+     `controls for main tree`=main_ps_ctrl,
+     `controls for Unr specific tree`=unr_ps_ctrl,
      `intersection with piRNA regulators`=te_regs_intersection,
      `intersection with ZADs`=zad_intersection,
      `ZAD enrichment test`=zad_fish)
